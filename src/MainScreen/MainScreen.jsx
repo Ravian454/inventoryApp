cb01b7b9993af641836a0f3d041369aba91b851b
Ali Hamza <73699762+Ravian454@users.noreply.github.com>
2024-04-27 16:32:52 +0500

    Added the barcode reader option and also added the navbar to the page

--- src/MainScreen/MainScreen.jsx
+++ src/MainScreen/MainScreen.jsx
@@ -1,96 +1,131 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { TextField, Button, Container, Grid, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import Navbar from '../Navbar/Navbar.jsx';
 
 const MainScreen = () => {
+  const [barcode, setBarcode] = useState('');
+  const barcodeRef = useRef(null);
+
+  useEffect(() => {
+    if (barcodeRef.current) {
+      barcodeRef.current.focus();
+    }
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     // Handle form submission logic here
   };
 
+  const handleBarcodeChange = (event) => {
+    setBarcode(event.target.value);
+  };
+
   return (
-    <Container maxWidth="sm" style={{ marginTop: '50px' }}>
-      <style>{'body { background-color: white; }'}</style>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="Category"
-              variant="outlined"
-              required
-              type="text"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <FormControl fullWidth>
-              <InputLabel id="size-label">Age</InputLabel>
-              <Select
-                labelId="size-label"
-                id="size"
-                label="Age"
-                defaultValue={10} // Default value for the select field
+    <>
+      <Navbar />
+      <Container maxWidth="sm" style={{ marginTop: '50px',  justifyContent: 'end', minHeight: 'calc(100vh - 64px)' }}>
+      <style>{`
+          body {
+            display: block; /* Reset body's display property */
+            background-color: white;
+          }
+        `}</style>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="Barcode"
+                variant="outlined"
+                type="text"
+                value={barcode}
+                onChange={handleBarcodeChange}
+                inputProps={{ 'data-testid': 'barcode-input' }}
+                inputRef={barcodeRef}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="Category"
+                variant="outlined"
+                required
+                type="text"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel id="size-label">Age</InputLabel>
+                <Select
+                  labelId="size-label"
+                  id="size"
+                  label="Age"
+                  defaultValue={10}
+                >
+                  <MenuItem value={10}>16 (1.5 to 2 years)</MenuItem>
+                  <MenuItem value={18}>18 (2 to 3 years)</MenuItem>
+                  <MenuItem value={20}>20 (3 to 4 years)</MenuItem>
+                  <MenuItem value={22}>22 (4 to 5 years)</MenuItem>
+                  <MenuItem value={24}>24 (5 to 6 years)</MenuItem>
+                  <MenuItem value={26}>26 (7 to 8 years)</MenuItem>
+                  <MenuItem value={28}>28 (8 to 9 years)</MenuItem>
+                  <MenuItem value={30}>30 (9 to 10 years)</MenuItem>
+                  <MenuItem value={32}>32 (10 to 11 years)</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="Names"
+                variant="outlined"
+                type="text"
+                required
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="Price"
+                variant="outlined"
+                type="number"
+                required
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="In Stock"
+                variant="outlined"
+                type="number"
+                required
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="Out Stock"
+                variant="outlined"
+                type="number"
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
               >
-                <MenuItem value={10}>16 (1.5 to 2 years)</MenuItem>
-                <MenuItem value={18}>18 (2 to 3 years)</MenuItem>
-                <MenuItem value={20}>20 (3 to 4 years)</MenuItem>
-                <MenuItem value={22}>22 (4 to 5 years)</MenuItem>
-                <MenuItem value={24}>24 (5 to 6 years)</MenuItem>
-                <MenuItem value={26}>26 (7 to 8 years)</MenuItem>
-                <MenuItem value={28}>28 (8 to 9 years)</MenuItem>
-                <MenuItem value={30}>30 (9 to 10 years)</MenuItem>
-                <MenuItem value={32}>32 (10 to 11 years)</MenuItem>
-              </Select>
-            </FormControl>
+                Submit
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="Names"
-              variant="outlined"
-              type="text" // Assuming names are text
-              required
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="Price"
-              variant="outlined"
-              type="number" // Assuming price is a number
-              required
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="In Stock"
-              variant="outlined"
-              type="number" // Assuming stock count is a number
-              required
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              fullWidth
-              label="Out Stock"
-              variant="outlined"
-              type="number" // Assuming out of stock count is a number
-              required
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              Submit
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
+        </form>
+      </Container>
+    </>
   );
 };
 
